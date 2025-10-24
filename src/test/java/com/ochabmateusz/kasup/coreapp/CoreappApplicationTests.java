b140eafbeab537cfa3288085d9b99b18af395fea
mateuszochab <ochab.mat@gmail.com>
2025-10-24 17:41:03 +0200

    initial commit

---
+++ src/test/java/com/ochabmateusz/kasup/coreapp/CoreappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ochabmateusz.kasup.coreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
